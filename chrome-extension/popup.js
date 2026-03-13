e4baaee4bd79fdb8dbf72ec62054cd8a4a27617f
Dharma Teja <dharmatejamuddana@gmail.com>
2026-03-13 21:26:12 +0530

    Updated

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -3,14 +3,14 @@ document.getElementById("checkBtn").addEventListener("click", async () => {
 let emailText = document.getElementById("emailText").value;
 
 if(!emailText){
-    document.getElementById("result").innerText="Please enter email text";
-    return;
+document.getElementById("result").innerText="Please enter email text";
+return;
 }
 
 try{
 
 let response = await fetch(
-"https://phishing-email-detection-ml.onrender.com/api/predict",
+"https://phishing-email-detection-0a7t.onrender.com/api/predict",
 {
 method:"POST",
 headers:{
@@ -26,13 +26,12 @@ let data = await response.json();
 if(data.success){
 document.getElementById("result").innerText =
 "Prediction: " + data.prediction;
-}
-else{
+}else{
 document.getElementById("result").innerText =
-"Error: "+data.error;
+"Error: " + data.error;
 }
 
-}catch(err){
+}catch(error){
 document.getElementById("result").innerText="Server error";
 }
 
